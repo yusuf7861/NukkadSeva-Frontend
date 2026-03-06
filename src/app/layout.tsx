d33927dc8e2b9bece34bad21e6edc5d9c14ffcf2
yusuf7861 <yjamal710@gmail.com>
2026-03-06 22:43:15 +0530

    font was not loading, now resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap" rel="stylesheet" />
             </head>
             <body className={interClassName}>
                 <GoogleOAuthProvider clientId={googleClientId}>
